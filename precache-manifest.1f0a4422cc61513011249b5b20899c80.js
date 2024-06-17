e08f3bb75ba1c63c51c2f07aa8186e6aea94ccd9
lishagreen55 <lishagreen55@gmail.com>
2024-06-17 12:48:14 -0500

    Effective People Management

---
+++ precache-manifest.1f0a4422cc61513011249b5b20899c80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60fb073570951fb15c7319bf11ebd03b",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "8f85875416ebde3226aa",
+    "url": "@yield('content-viewer-url')/static/css/2.37be29a2.chunk.css"
+  },
+  {
+    "revision": "b4196d058e0578513b26",
+    "url": "@yield('content-viewer-url')/static/css/main.51e1c032.chunk.css"
+  },
+  {
+    "revision": "8f85875416ebde3226aa",
+    "url": "@yield('content-viewer-url')/static/js/2.979a5880.chunk.js"
+  },
+  {
+    "revision": "cfb333affc1e9e4e40ea7a1e949500b6",
+    "url": "@yield('content-viewer-url')/static/js/2.979a5880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4196d058e0578513b26",
+    "url": "@yield('content-viewer-url')/static/js/main.f431a86b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "@yield('content-viewer-url')/static/js/main.f431a86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33a12fe361989d2d462",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.eac69593.js"
+  },
+  {
+    "revision": "a754c5ed00d8f6cca2a96794a2beaddc",
+    "url": "@yield('content-viewer-url')/static/media/trophy.a754c5ed.svg"
+  }
+]);
